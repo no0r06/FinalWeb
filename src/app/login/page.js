3cf6c7204fc891088ee299684207daffb5e7f8c0
noor <noorzohbi8@gmail.com>
2026-05-23 11:21:29 +0300

    Project fixing

---
+++ src/app/login/page.js
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <main style={{ padding: "2rem" }}>
+      <h1>Login</h1>
+      <p>This page is a placeholder until the login UI is implemented.</p>
+    </main>
+  );
+}
